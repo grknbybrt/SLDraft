bb2a996d813565e86643eefae8e464a37e200cb6
grknbybrt <168705431+grknbybrt@users.noreply.github.com>
2025-05-22 12:49:06 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html')
-    }
+    outDir: 'dist'
   }
-});
+})
